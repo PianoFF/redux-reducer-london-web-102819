4866faaf51b5b703bdd37cc47717bd7209bf2d31
PianoFF <Fangfei1star@gmail.com>
2020-03-09 18:14:41 +0000

    added default state in parameters

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,4 +1,9 @@
-export function manageFriends(state, action) {
+export function manageFriends(
+  state = {
+    friends: []
+  },
+  action
+) {
   switch (action.type) {
     case "ADD_FRIEND":
       return { friends: [...state.friends, action.friend] };
